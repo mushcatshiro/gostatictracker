d00de8fcb4e51ebadcc82e2aee29e3e2c88da7aa
mushcatshiro <yingjieho@hotmail.com>
2025-07-20 20:10:35 +0800

    add gantt render

---
+++ render/gantt.go
@@ -0,0 +1,380 @@
+package render
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mushcatshiro/gostatictracker/dbop"
+)
+
+const TimeLayout = "01-02-2006" // MM-DD-YYYY
+
+type eventGanttHeader struct {
+	RectX     int
+	RectWidth int
+	TextX     int
+	TextY     int
+	TextVal   string
+	DataDate  string
+}
+
+type eventGanttRow struct {
+	RectX       int
+	RectY       int
+	RectWidth   int
+	DataDetails string
+	LineX1      int
+	LineY1      int
+	LineX2      int
+	LineY2      int
+	TextX       int
+	TextY       float32
+	TextVal     string
+}
+
+type eventGanttBase struct {
+	HeaderRectWidth  int
+	HeaderRectHeight int
+	RowRectHeight    int
+	SvgWidth         int
+	SvgHeight        int
+	Group            string
+	Years            []eventGanttHeader
+	Months           []eventGanttHeader
+	Days             []eventGanttHeader
+	Rows             []eventGanttRow
+}
+
+type ganttRenderMetadata struct {
+	isDayView            bool
+	fullTaskDuration     int
+	groupStartTime       time.Time
+	groupEndTime         time.Time
+	groupName            string
+	rowTextInRectPadding int     // LR padding for text to be place in rect
+	rectToTextMargin     int     // spacing between rect and text
+	textYOffset          float32 // approximate text Y offset from Y (center)
+	rowRectMargin        int     // spacing between two rect/row
+	rowRectHeight        int
+	headersOffset        int
+	baseHeaderRectWidth  int // date or week rect width
+	headerRectHeight     int
+	headerRectMargin     int
+	headerTextYOffset    int // somehow it needs +2 after dividing height by 2
+}
+
+func getGanttRenderMetadata(db *sql.DB, groupName string) (ganttRenderMetadata, error) {
+	g := ganttRenderMetadata{
+		isDayView:            true,
+		groupName:            groupName,
+		rowTextInRectPadding: 4,
+		rectToTextMargin:     2,
+		textYOffset:          6.3,
+		rowRectMargin:        2,
+		rowRectHeight:        10,
+		headersOffset:        (24 + 2) * 3,
+		baseHeaderRectWidth:  30,
+		headerRectHeight:     24,
+		headerRectMargin:     2,
+		headerTextYOffset:    2,
+	}
+	query := `SELECT
+		TO_CHAR(MIN(d), 'MM-DD-YYYY') AS startDate,
+		TO_CHAR(MAX(d), 'MM-DD-YYYY') AS endDate,
+		EXTRACT(DAY FROM MAX(d) - MIN(d)) + 1 AS taskFullDuration
+	FROM (
+		SELECT "start" d FROM events WHERE "group" = $1
+		UNION ALL
+		SELECT "end" d FROM events WHERE "group" = $1
+	)`
+	row := db.QueryRow(query, groupName)
+	var groupStartTime, groupEndTime string
+	if err := row.Scan(&groupStartTime, &groupEndTime, &g.fullTaskDuration); err != nil {
+		return ganttRenderMetadata{}, fmt.Errorf("\nfailed to scan start/end time and full duration:\n%w", err)
+	}
+	var tGroupStartTime, tGroupEndTime time.Time
+	tGroupStartTime, err := time.Parse(TimeLayout, groupStartTime)
+	if err != nil {
+		return g, fmt.Errorf("\nfailed to parse start time: %w", err)
+	}
+	tGroupEndTime, err = time.Parse(TimeLayout, groupEndTime)
+	if err != nil {
+		return g, fmt.Errorf("\nfailed to parse end time: %w", err)
+	}
+	g.groupStartTime = tGroupStartTime
+	g.groupEndTime = tGroupEndTime
+
+	query = `SELECT MIN(EXTRACT(DAY FROM "end" - start)) AS minTaskDuration FROM events WHERE "group" = $1`
+	var minTaskDuration sql.NullInt64
+	err = db.QueryRow(query, groupName).Scan(&minTaskDuration)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return ganttRenderMetadata{}, fmt.Errorf("\nno events found for group %s:\n%w", groupName, err)
+		}
+		log.Printf("Failed to scan minimum task duration: %v; using default day view", err)
+	} else if minTaskDuration.Valid && minTaskDuration.Int64 > 7 {
+		g.isDayView = false
+	}
+	return g, nil
+}
+
+func getTextEstimateWidth(text string) int {
+	return len([]rune(text)) * 7
+}
+
+func getRowRectWidth(startTime time.Time, endTime time.Time, headerWidthWithSpacing int) int {
+	daysSpan := int(endTime.Sub(startTime).Hours()/24) + 1 // include starting day
+	return daysSpan * headerWidthWithSpacing
+}
+
+func getGanttEventRows(events []dbop.Event, g ganttRenderMetadata, debug bool) ([]eventGanttRow, int, error) {
+	// TODO
+	// support actual start, actual end rendering including start, stop button
+	// refactor function
+	rSlice := []eventGanttRow{}
+
+	var maxWidth, rowEndWidth int
+	var overFlowDays float64
+
+	lineY1 := g.headersOffset - 2
+	lineY2 := g.headersOffset + len(events)*(g.rowRectHeight+g.rowRectMargin)
+
+	for idx, event := range events {
+		iStartTime, err := time.Parse(TimeLayout, event.Start)
+		if err != nil {
+			return rSlice, -1, fmt.Errorf("not able to parse start time from events[%v]: %w", idx, err)
+		}
+		iEndTime, err := time.Parse(TimeLayout, event.End)
+		if err != nil {
+			return rSlice, -1, fmt.Errorf("not able to parse end time from events[%v]: %w", idx, err)
+		}
+
+		duration := iStartTime.Sub(g.groupStartTime)
+		daysSpacing := int(duration.Hours() / 24)
+
+		titleWidth := getTextEstimateWidth(event.Title)
+		rowRectWidth := getRowRectWidth(iStartTime, iEndTime, g.baseHeaderRectWidth+g.headerRectMargin)
+
+		var rectX, rectY, textX, lineX1, lineX2 int
+		var textY float32
+
+		rectX = daysSpacing * (g.baseHeaderRectWidth + g.headerRectMargin)
+		rectY = (idx * (g.rowRectHeight + g.rowRectMargin)) + g.headersOffset
+		textY = float32(rectY) + g.textYOffset
+		rowEndWidth = max(rowEndWidth, rectX+rowRectWidth)
+		if rowRectWidth-g.rowRectMargin < titleWidth {
+			// BUG titleWidth might not be divisible by 2 and will always floored, might get inconsistent spacing
+			textX = rectX + rowRectWidth + g.rectToTextMargin + (titleWidth / 2)
+			maxWidth = max(maxWidth, rectX+rowRectWidth+(g.rectToTextMargin*2)+titleWidth)
+		} else {
+			textX = (rowRectWidth / 2) + rectX
+			maxWidth = max(maxWidth, rectX+rowRectWidth+g.rectToTextMargin)
+		}
+		lineX1 = rectX + rowRectWidth
+		lineX2 = rectX + rowRectWidth
+
+		var description string
+		if debug {
+			// BUG for now
+			description = event.Description + "\n" + event.Start + "\n" + event.End
+		} else {
+			description = event.Description + "\n" + event.Start + "\n" + event.End
+		}
+		e := eventGanttRow{
+			RectX:       rectX,
+			RectY:       rectY,
+			RectWidth:   rowRectWidth,
+			DataDetails: description,
+			LineX1:      lineX1,
+			LineY1:      lineY1,
+			LineX2:      lineX2,
+			LineY2:      lineY2,
+			TextX:       textX,
+			TextY:       textY,
+			TextVal:     event.Title,
+		}
+		rSlice = append(rSlice, e)
+	}
+	var retOverFlowDays int
+	overFlowDays = float64(maxWidth-rowEndWidth) / float64(g.baseHeaderRectWidth+g.headerRectMargin)
+	if overFlowDays < 0.2 {
+		retOverFlowDays = 0
+	} else {
+		retOverFlowDays = int(math.Ceil(overFlowDays))
+	}
+	return rSlice, retOverFlowDays, nil
+}
+
+func getGanttHeaders(g ganttRenderMetadata, overflowDays int) ([]eventGanttHeader, []eventGanttHeader, []eventGanttHeader) {
+	startTime := g.groupStartTime
+	endTime := g.groupEndTime.AddDate(0, 0, overflowDays)
+
+	yearGanttHeader := []eventGanttHeader{}
+	monthGanttHeader := []eventGanttHeader{}
+	dayGanttHeader := []eventGanttHeader{}
+
+	var iterYear, iterDay, dIdx, monthRectEndX, yearRectEndX int
+	var cumDaysForMonth, cumDaysForYear int
+	var iterMonth time.Month
+
+	for !startTime.After(endTime) {
+		iterYear, iterMonth, iterDay = startTime.Date()
+
+		d := eventGanttHeader{
+			RectX:     dIdx * (g.baseHeaderRectWidth + g.headerRectMargin),
+			RectWidth: g.baseHeaderRectWidth,
+			TextX:     dIdx*(g.baseHeaderRectWidth+g.headerRectMargin) + int(g.baseHeaderRectWidth/2),
+			TextY:     2*(g.headerRectHeight+g.headerRectMargin) + int(g.headerRectHeight/2) + g.headerTextYOffset,
+			TextVal:   strconv.Itoa(iterDay),
+			DataDate:  startTime.Format("2006-01-02"), // YYYY-MM-DD
+		}
+		dayGanttHeader = append(dayGanttHeader, d)
+		dIdx++
+		cumDaysForMonth++
+		cumDaysForYear++
+
+		if startTime.AddDate(0, 0, 1).Month() != startTime.Month() {
+			currentMonthWidth := (cumDaysForMonth * (g.baseHeaderRectWidth + g.headerRectMargin)) - g.headerRectMargin
+			m := eventGanttHeader{
+				RectX:     monthRectEndX,
+				RectWidth: currentMonthWidth,
+				TextX:     monthRectEndX + int(currentMonthWidth/2),
+				TextY:     (g.headerRectHeight + g.headerRectMargin) + int(g.headerRectHeight/2) + g.headerTextYOffset,
+				TextVal:   strconv.Itoa(int(iterMonth)),
+			}
+			monthGanttHeader = append(monthGanttHeader, m)
+			monthRectEndX += currentMonthWidth + g.headerRectMargin
+			cumDaysForMonth = 0
+		}
+		if startTime.AddDate(0, 0, 1).Year() != startTime.Year() {
+			currentYearWidth := (cumDaysForYear * (g.baseHeaderRectWidth + g.headerRectMargin)) - g.headerRectMargin
+			y := eventGanttHeader{
+				RectX:     yearRectEndX,
+				RectWidth: currentYearWidth,
+				TextX:     yearRectEndX + int(currentYearWidth/2),
+				TextY:     int(g.headerRectHeight/2) + g.headerTextYOffset,
+				TextVal:   strconv.Itoa(iterYear)}
+			yearGanttHeader = append(yearGanttHeader, y)
+			yearRectEndX += currentYearWidth + g.headerRectMargin
+			cumDaysForYear = 0
+		}
+
+		startTime = startTime.AddDate(0, 0, 1)
+	}
+	if cumDaysForMonth > 0 {
+		currentMonthWidth := (cumDaysForMonth * (g.baseHeaderRectWidth + g.headerRectMargin)) - g.headerRectMargin
+		m := eventGanttHeader{
+			RectX:     monthRectEndX,
+			RectWidth: currentMonthWidth,
+			TextX:     monthRectEndX + int(currentMonthWidth/2),
+			TextY:     (g.headerRectHeight + g.headerRectMargin) + int(g.headerRectHeight/2) + g.headerTextYOffset,
+			TextVal:   strconv.Itoa(int(iterMonth)),
+		}
+		monthGanttHeader = append(monthGanttHeader, m)
+	}
+	if cumDaysForYear > 0 {
+		currentYearWidth := (cumDaysForYear * (g.baseHeaderRectWidth + g.headerRectMargin)) - g.headerRectMargin
+		y := eventGanttHeader{
+			RectX:     yearRectEndX,
+			RectWidth: currentYearWidth,
+			TextX:     yearRectEndX + int(currentYearWidth/2),
+			TextY:     int(g.headerRectHeight/2) + g.headerTextYOffset,
+			TextVal:   strconv.Itoa(iterYear)}
+		yearGanttHeader = append(yearGanttHeader, y)
+	}
+	return yearGanttHeader, monthGanttHeader, dayGanttHeader
+}
+
+func getGanttEventBase(events []dbop.Event, padding int, g ganttRenderMetadata, debug bool) (eventGanttBase, error) {
+	// allow headerRectWidth, headerRectHeight, rowRectHeight to have default values
+	e := eventGanttBase{
+		HeaderRectWidth:  g.baseHeaderRectWidth,
+		HeaderRectHeight: g.headerRectHeight,
+		RowRectHeight:    g.rowRectHeight,
+		Group:            g.groupName,
+	}
+	e.SvgHeight = g.headersOffset + len(events)*(e.RowRectHeight+padding)
+
+	rSlice, overflowDays, err := getGanttEventRows(events, g, debug)
+	if err != nil {
+		return eventGanttBase{}, err
+	}
+	e.Rows = rSlice
+
+	e.SvgWidth = (g.fullTaskDuration+overflowDays)*(e.HeaderRectWidth+g.headerRectMargin) + 1 // buffer
+
+	y, m, d := getGanttHeaders(g, overflowDays)
+
+	e.Years = y
+	e.Months = m
+	e.Days = d
+	return e, nil
+}
+
+func renderGanttHTML(events []dbop.Event, file *os.File, g ganttRenderMetadata, debug bool) error {
+	var t *template.Template
+	var err error
+
+	if g.isDayView {
+		// t, err = template.ParseFiles("dayGantt.html")
+		t, err = template.ParseGlob(filepath.Join(".", "day*.html"))
+	} else {
+		// t, err = template.ParseFiles("weekGantt.html")
+		return fmt.Errorf("not supporting week view gantt")
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to create template:\n\t%w", err)
+	}
+	data, err := getGanttEventBase(events, 2, g, debug)
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("%+v\n", data)
+	err = t.Execute(file, data)
+	return err
+}
+
+func RenderGantt(renderTargetPath string, conn *sql.DB, debug bool) {
+	groups, err := dbop.GetUniqueGroups(conn)
+	if err != nil {
+		log.Fatalf("Not able to query any group(s): %v", err)
+	}
+	for _, group := range groups {
+		log.Printf("Processing group: %s", group)
+		events, err := dbop.GetGanttGroupEvents(conn, group, true)
+		if err != nil {
+			log.Printf("Failed to get gantt group events: %v", err)
+			continue
+		}
+		fileName := strings.Replace(group, " ", "-", -1) + "-gantt.html"
+		file, err := os.Create(renderTargetPath + "/" + fileName) // truncates if exists
+		if err != nil {
+			log.Printf("Failed to create file %s: %v", fileName, err)
+			continue
+		}
+		/*
+			align to go idiom, only `Close` when file is created successfully;
+			when err != nil, file is likely `nil`
+			`nil.Close()` is potentially nil pointer dereference
+		*/
+		defer file.Close()
+
+		g, err := getGanttRenderMetadata(conn, group)
+		if err != nil {
+			log.Fatalf("Failed to get gantt render metadata for group %s: %v", group, err)
+		}
+		err = renderGanttHTML(events, file, g, debug)
+		if err != nil {
+			log.Printf("Failed to process %s:\n\t%v", fileName, err)
+		}
+	}
+}
---
+++ render/gantt_test.go
@@ -0,0 +1,208 @@
+package render
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mushcatshiro/gostatictracker/mock"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTextEstimateWidth(t *testing.T) {
+	assert.Equal(t, 4*7, getTextEstimateWidth("Test"))
+	assert.Equal(t, 5*7, getTextEstimateWidth("Test "))
+}
+
+func TestGetRowRectWidth(t *testing.T) {
+	t1, _ := time.Parse(TimeLayout, "12-27-2024")
+	t2, _ := time.Parse(TimeLayout, "12-29-2024")
+	t.Run("", func(t *testing.T) {
+		result := getRowRectWidth(t1, t2, 26)
+		assert.Equal(t, 3*26, result)
+	})
+
+	t3, _ := time.Parse(TimeLayout, "12-27-2024")
+	t4, _ := time.Parse(TimeLayout, "12-27-2024")
+	t.Run("", func(t *testing.T) {
+		result := getRowRectWidth(t3, t4, 26)
+		assert.Equal(t, 1*26, result)
+	})
+}
+
+func TestGetGanttEventRowsDayView(t *testing.T) {
+	// hardcoded `ganttRenderMetadata`
+	var groupStartTime, groupEndTime time.Time
+	groupStartTime, _ = time.Parse(TimeLayout, mock.MockData[0].Start)
+	groupEndTime, _ = time.Parse(TimeLayout, mock.MockData[6].End)
+	g0 := ganttRenderMetadata{
+		isDayView:            true,
+		fullTaskDuration:     10,
+		groupStartTime:       groupStartTime,
+		groupEndTime:         groupEndTime,
+		groupName:            "test",
+		rowTextInRectPadding: 4,
+		rectToTextMargin:     2,
+		textYOffset:          6.3,
+		rowRectMargin:        2,
+		rowRectHeight:        10,
+		headersOffset:        (24 + 2) * 3,
+		baseHeaderRectWidth:  30,
+		headerRectHeight:     24,
+		headerRectMargin:     2,
+		headerTextYOffset:    2,
+	}
+
+	// trick for converting a defined size array into a slice
+	r0 := eventGanttRow{
+		RectX:       0,
+		RectY:       78,
+		RectWidth:   192,
+		DataDetails: "This is a mock event for testing purposes.\n12-27-2024\n01-01-2025",
+		LineX1:      192,
+		LineY1:      76,
+		LineX2:      192,
+		LineY2:      162,
+		TextX:       96,
+		TextY:       84.3,
+		TextVal:     "Mock task 1",
+	}
+	r1 := eventGanttRow{
+		RectX:       64,
+		RectY:       90,
+		RectWidth:   160,
+		DataDetails: "This is a mock event for testing purposes.\n12-29-2024\n01-02-2025",
+		LineX1:      224,
+		LineY1:      76,
+		LineX2:      224,
+		LineY2:      162,
+		TextX:       144,
+		TextY:       96.3,
+		TextVal:     "Mock task 2",
+	}
+	r2 := eventGanttRow{
+		RectX:       128,
+		RectY:       102,
+		RectWidth:   160,
+		DataDetails: "This is a mock event for testing purposes.\n12-31-2024\n01-04-2025",
+		LineX1:      288,
+		LineY1:      76,
+		LineX2:      288,
+		LineY2:      162,
+		TextX:       208,
+		TextY:       108.3,
+		TextVal:     "Mock task 3",
+	}
+	r3 := eventGanttRow{
+		RectX:       128,
+		RectY:       114,
+		RectWidth:   192,
+		DataDetails: "This is a mock event for testing purposes.\n12-31-2024\n01-05-2025",
+		LineX1:      320,
+		LineY1:      76,
+		LineX2:      320,
+		LineY2:      162,
+		TextX:       224,
+		TextY:       120.3,
+		TextVal:     "Mock task 4",
+	}
+	r4 := eventGanttRow{
+		RectX:       128,
+		RectY:       126,
+		RectWidth:   160,
+		DataDetails: "This is a mock event for testing purposes.\n12-31-2024\n01-04-2025",
+		LineX1:      288,
+		LineY1:      76,
+		LineX2:      288,
+		LineY2:      162,
+		TextX:       208,
+		TextY:       132.3,
+		TextVal:     "Mock task 5",
+	}
+	r5 := eventGanttRow{
+		RectX:       96,
+		RectY:       138,
+		RectWidth:   224,
+		DataDetails: "This is a mock event for testing purposes.\n12-30-2024\n01-05-2025",
+		LineX1:      320,
+		LineY1:      76,
+		LineX2:      320,
+		LineY2:      162,
+		TextX:       208,
+		TextY:       144.3,
+		TextVal:     "Mock task 6",
+	}
+	r6 := eventGanttRow{
+		RectX:       32,
+		RectY:       150,
+		RectWidth:   288,
+		DataDetails: "This is a mock event for testing purposes.\n12-28-2024\n01-05-2025",
+		LineX1:      320,
+		LineY1:      76,
+		LineX2:      320,
+		LineY2:      162,
+		TextX:       176,
+		TextY:       156.3,
+		TextVal:     "Mock task 7",
+	}
+
+	t.Run("base case without overflow `getGanttEventRows`", func(t *testing.T) {
+		resultGanttEventRow, resultOverflowDay, err := getGanttEventRows(mock.MockData[:7], g0, false)
+		assert.NoError(t, err, "Not expecting parsing related error: %v", err)
+		assert.Equal(t, 0, resultOverflowDay)
+		assert.Equal(t, 7, len(resultGanttEventRow))
+		assert.Equal(t, r0, resultGanttEventRow[0])
+		assert.Equal(t, r1, resultGanttEventRow[1])
+		assert.Equal(t, r2, resultGanttEventRow[2])
+		assert.Equal(t, r3, resultGanttEventRow[3])
+		assert.Equal(t, r4, resultGanttEventRow[4])
+		assert.Equal(t, r5, resultGanttEventRow[5])
+		assert.Equal(t, r6, resultGanttEventRow[6])
+	})
+
+	groupStartTime, _ = time.Parse(TimeLayout, mock.MockData[7].Start)
+	groupEndTime, _ = time.Parse(TimeLayout, mock.MockData[7].End)
+	g1 := ganttRenderMetadata{
+		isDayView:            true,
+		fullTaskDuration:     2,
+		groupStartTime:       groupStartTime,
+		groupEndTime:         groupEndTime,
+		groupName:            "test",
+		rowTextInRectPadding: 4,
+		rectToTextMargin:     2,
+		textYOffset:          6.3,
+		rowRectMargin:        2,
+		rowRectHeight:        10,
+		headersOffset:        (24 + 2) * 3,
+		baseHeaderRectWidth:  30,
+		headerRectHeight:     24,
+		headerRectMargin:     2,
+		headerTextYOffset:    2,
+	}
+
+	r7 := eventGanttRow{
+		RectX:       0,
+		RectY:       78,
+		RectWidth:   64,
+		DataDetails: "This is a mock event for testing purposes.\n01-04-2025\n01-05-2025",
+		LineX1:      64,
+		LineY1:      76,
+		LineX2:      64,
+		LineY2:      90,
+		TextX:       104,
+		TextY:       84.3,
+		TextVal:     "Mock task 1",
+	}
+	t.Run("base case without overflow `getGanttEventRows`", func(t *testing.T) {
+		resultGanttEventRow, resultOverflowDay, err := getGanttEventRows(mock.MockData[7:], g1, false)
+		assert.NoError(t, err, "Not expecting parsing related error: %v", err)
+		assert.Equal(t, 3, resultOverflowDay)
+		assert.Equal(t, 1, len(resultGanttEventRow))
+		assert.Equal(t, r7, resultGanttEventRow[0])
+	})
+
+}
+
+func TestGetGanttHeadersDayView(t *testing.T) {
+	// getGanttHeaders()
+}

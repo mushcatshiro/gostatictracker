bb370fa295fd3d6de6a8e90afcb23be9e9809e8e
mushcatshiro <yingjieho@hotmail.com>
2025-09-16 07:14:58 +0800

    calendar tmpl

--- render/calendarTemplate.go
+++ render/calendarTemplate.go
@@ -1,10 +1,41 @@
 package render
 
-var calendarHeader elm = elm{}
+var calendarHeader elm = elm{
+	tag: "html",
+	childs: []elm{{
+			tag: "style",
+			innerText: `
+			.calendar-container {
+				display: grid;
+				grid-template-columns: repeat(7, 1fr);
+			}
+			.day-cell {
+				border: 1px solid #ccc,
+				padding: 5px;
+				min-height: 100px;
+			}
+			.event-div {
+				grid-column-start: var(--start-col);
+				grid-row-start: var(--start-row);
+				grid-column-end: span var(--span);
+				background-color: #4A90E2;
+				color: white;
+				margin-top: 25px;
+				padding: 2px, 5px;
+				overflow: hidden;
+				z-index: 1;
+			}
+		`,
+	},},
+}
 
-var calendarBody elm = elm{}
+var calendarBody elm = elm{
+	tag: "body",
+}
 
 var calendarContainer elm = elm{
-	tag: "div",
-	attrs: attrsStruct{style: "display: grid",},
+	tag:   "div",
+	attrs: attrsStruct{
+		class: "calendar-container",
+	},
 }

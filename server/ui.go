6d753429d173c112ebe1879f086bb5649df06a25
mushcatshiro <yingjieho@hotmail.com>
2025-09-21 22:57:22 +0800

    rename

--- server/ui.go
+++ server/ui.go
@@ -1,11 +1,13 @@
-package main
+package server
 
 import (
 	"database/sql"
-	_ "embed"
+	"embed"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"net/url"
@@ -17,6 +19,9 @@ var conn *sql.DB
 
 //go:embed ganttui.html
 var ui []byte
+
+//go:embed web/*
+var content embed.FS
 
 type groups struct {
 	GroupName string `json:"groupName"`
@@ -80,9 +85,9 @@ func handleAddEvent(w http.ResponseWriter, r *http.Request) {
 			// TODO create gs obj and encode method
 			gs, err := dbop.GetUniqueGroups(conn)
 			if err != nil {
-				http.Error(w, "no groups found", http.StatusNotFound)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
 			}
-			var gstruct []groups
+			gstruct := []groups{}
 			for _, g := range gs {
 				t := groups{GroupName: g, ActionURL: "/events?group=" + url.QueryEscape(g)}
 				gstruct = append(gstruct, t)
@@ -100,11 +105,30 @@ func handleAddEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handleUngrouppedEvent(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		ungrouppedEvents, err := dbop.GetUngrouppedEvents(conn)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
+		es := []dbop.Event{}
+		for _, e := range ungrouppedEvents {
+			t := dbop.Event{ID: e.ID, Title: e.Title}
+			es = append(es, t)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(es); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
 func handlerUI(w http.ResponseWriter, r *http.Request) {
 	w.Write(ui)
 }
 
-func main() {
+func Run() {
 	var username = flag.String("dbUser", "pgsql", "The postgresql username")
 	var password = flag.String("dbPass", "pgsql", "The postgresql password")
 	var dbHost = flag.String("dbNost", "localhost", "The postgresql host IP")
@@ -123,9 +147,50 @@ func main() {
 	defer conn.Close()
 	dbop.InitDB(conn)
 
+	webFS, err := fs.Sub(content, "web")
+	if err != nil {
+		log.Fatalf("failed to create sub filesystem: %v", err)
+	}
 	http.HandleFunc("/events", handleAddEvent)
 	http.HandleFunc("/UI", handlerUI)
 	http.HandleFunc("/healthcheck", handlerHealthCheck)
+	http.HandleFunc("/UIv2", func(w http.ResponseWriter, r *http.Request) {
+		// Corrected way to read file from fs.FS
+		file, err := webFS.Open("index.html")
+		if err != nil {
+			http.Error(w, "Could not open index.html", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close() // Ensure the file is closed
+
+		htmlBytes, err := io.ReadAll(file) // Read all content from the file
+		if err != nil {
+			http.Error(w, "Could not read index.html content", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(htmlBytes)
+	})
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		// Corrected way to read file from fs.FS
+		file, err := webFS.Open("test.html")
+		if err != nil {
+			http.Error(w, "Could not open test.html", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close() // Ensure the file is closed
+
+		htmlBytes, err := io.ReadAll(file) // Read all content from the file
+		if err != nil {
+			http.Error(w, "Could not read index.html content", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(htmlBytes)
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(webFS))))
 	fmt.Printf("Server running on http://%s:%s\n", *serverHost, *serverPort)
 	addr := *serverHost + ":" + *serverPort
 	log.Fatal(http.ListenAndServe(addr, nil))

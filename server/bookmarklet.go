2cd62f86f565b52b322ffb2a469dd50f4c8b9beb
mushcatshiro <yingjieho@hotmail.com>
2025-09-28 07:39:00 +0800

    rm write

--- server/bookmarklet.go
+++ server/bookmarklet.go
@@ -36,13 +36,12 @@ func (s *Server) handleInsertBookmarklet() http.HandlerFunc {
 		}
 
 		b := bp.ToBookmarklet()
-		eid, err := dbop.InsertEvent(s.db, b.ToEvent())
+		_, err := dbop.InsertEvent(s.db, b.ToEvent())
 		if err != nil {
 			errMsg := fmt.Sprintf("failed to insert with error: %v", err)
 			http.Error(w, errMsg, http.StatusBadRequest)
 		} else {
 			w.WriteHeader(http.StatusCreated)
-			fmt.Fprintf(w, `{"eid": %d}`, eid)
 		}
 	}
 }

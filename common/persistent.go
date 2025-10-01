7074e10fd587d264130ff022bd7935b820774ea4
mushcatshiro <yingjieho@hotmail.com>
2025-10-01 22:32:03 +0800

    move to common

--- common/persistent.go
+++ common/persistent.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -14,6 +15,20 @@ func PersistToFileSystem(targetDirectory, fname, fileContent string) error {
 	_, err = file.Write([]byte(fileContent))
 	if err != nil {
 		return fmt.Errorf("failed to write to file: %v", err)
+	}
+	return nil
+}
+
+func ValidateInputDirectory(targetDirectory string) error {
+	info, err := os.Stat(targetDirectory)
+	if errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("Directory specified does not exists: %s", targetDirectory)
+	}
+	if err != nil {
+		return fmt.Errorf("Error during directory check %v", err)
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("Expects a valid path instead got %s", targetDirectory)
 	}
 	return nil
 }

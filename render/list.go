bcacb12b529fcf6b5d48f173de7cdb9f078b150c
shiro <yingjieho@hotmail.com>
2025-10-07 09:41:39 +0800

    simplify

--- render/list.go
+++ render/list.go
@@ -17,18 +17,15 @@ func buildElmTree(listEntries []models.ListEntry, groupName string) elm {
 		unorderedListElm.childs = append(unorderedListElm.childs, entryElm)
 	}
 
-	htmlBody := listBody
+	htmlBody := bodyElm
 	htmlBody.childs = append(htmlBody.childs, elm{tag: "h2", innerText: groupName})
 	htmlBody.childs = append(htmlBody.childs, unorderedListElm)
-	listHtmlElmTree := listHeader
-	listHtmlElmTree.childs = append(listHtmlElmTree.childs, htmlBody)
-	listHtmlElmTree.childs = append(listHtmlElmTree.childs, listScript)
 
-	return listHtmlElmTree
+	return buildBaseHtml(listStyleString, htmlBody, listScript)
 }
 
-func RenderListHTML(listEntries []models.ListEntry, groupName string) (string, error) {
+func RenderListHTML(listEntries []models.ListEntry, groupName string) string {
 	listHtmlElmTree := buildElmTree(listEntries, groupName)
 	htmlString := h(listHtmlElmTree)
-	return htmlString, nil
+	return htmlString
 }

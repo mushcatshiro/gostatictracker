3bd937ee3ab49e65ecffa5c5e9aeeb15c3cf7c95
mushcatshiro <yingjieho@hotmail.com>
2025-10-17 22:40:12 +0800

    fix spacing

--- render/common.go
+++ render/common.go
@@ -1,16 +1,17 @@
 package render
 
 func buildBaseHtml(styleString string, bodyElm, scriptElm elm) elm {
-    // support slice of scriptElm and styleElm instead of current mode
+	// support slice of scriptElm and styleElm instead of current mode
 	htmlBase := htmlElm
 
-    styleElm := elm{tag: "style", innerText: styleString}
+	baseStyleString := baseHtmlStyleString
+	styleElm := elm{tag: "style", innerText: baseStyleString+styleString}
 	htmlBase.childs = append(htmlBase.childs, styleElm)
 
 	htmlBase.childs = append(htmlBase.childs, bodyElm)
 
-    if scriptElm.tag != "" {
-        htmlBase.childs = append(htmlBase.childs, scriptElm)
-    }
+	if scriptElm.tag != "" {
+		htmlBase.childs = append(htmlBase.childs, scriptElm)
+	}
 	return htmlBase
 }

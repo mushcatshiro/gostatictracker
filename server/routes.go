117cbcd64c8609d9e82e4a39df74418d61c7d719
mushcatshiro <yingjieho@hotmail.com>
2025-10-20 21:26:19 +0800

    protect

--- server/routes.go
+++ server/routes.go
@@ -8,5 +8,5 @@ func (s *Server) RegisterRoutes() {
 	s.router.HandleFunc("/gantt", s.renderGanttView)
 	s.router.HandleFunc("/list", s.renderListView())
 	s.router.HandleFunc("/auth/google/callback", s.handleGoogleCallback())
-	s.router.HandleFunc("/bookmarkletsetup", s.renderBookmarkletSetup())
+	s.router.HandleFunc("/bookmarkletsetup", s.authMiddleware(s.renderBookmarkletSetup()))
 }

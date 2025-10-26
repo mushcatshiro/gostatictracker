1cdad6f0608bc6a4fb105b1c647ea2612f3bf1aa
mushcatshiro <yingjieho@hotmail.com>
2025-10-26 20:40:25 +0800

    add simple view to unify site

--- render/common.go
+++ render/common.go
@@ -5,7 +5,7 @@ func buildBaseHtml(styleString string, bodyElm, scriptElm elm) elm {
 	htmlBase := htmlElm
 
 	baseStyleString := baseHtmlStyleString
-	styleElm := elm{tag: "style", innerText: baseStyleString+styleString}
+	styleElm := elm{tag: "style", innerText: baseStyleString + styleString}
 	htmlBase.childs = append(htmlBase.childs, styleElm)
 
 	htmlBase.childs = append(htmlBase.childs, bodyElm)
@@ -14,4 +14,23 @@ func buildBaseHtml(styleString string, bodyElm, scriptElm elm) elm {
 		htmlBase.childs = append(htmlBase.childs, scriptElm)
 	}
 	return htmlBase
+}
+
+func RenderSimpleView(title, textBlock string) string {
+	var f, t, tb, e elm
+	f.tag = "div"
+
+	if title != "" {
+		t.tag = "h2"
+		t.innerText = title
+		f.childs = append(f.childs, t)
+	}
+	if textBlock != "" {
+		tb.tag = "p"
+		tb.innerText = textBlock
+		f.childs = append(f.childs, tb)
+	}
+
+	simpleElm := buildBaseHtml("", f, e)
+	return h(simpleElm)
 }

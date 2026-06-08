0fc028d2752cef9d4819f76d85ff850b278c8e86
mushcatshiro <yingjieho@hotmail.com>
2026-06-08 22:04:54 +0800

    rename

--- blog/blog_op.go
+++ blog/blog_op.go
@@ -7,9 +7,10 @@ import (
 	"path/filepath"
 
 	"github.com/mushcatshiro/gostatictracker/models"
+	"github.com/spf13/afero"
 )
 
-func (bm *BlogManager) CreatePage(path string, content string) error {
+func (bm *BlogManager) CreateNewBlog(path string, content string) error {
 	err := bm.Fs.MkdirAll(filepath.Dir(path), 0644)
 	if err != nil {
 		return err
@@ -26,29 +27,38 @@ func (bm *BlogManager) CreatePage(path string, content string) error {
 	return nil
 }
 
-func (bm *BlogManager) GetSpecificPageByPath(path string, contentOnly bool) (models.BlogEntry, error) {
+func (bm *BlogManager) GetSpecificBlogByPath(path string, contentOnly bool) (models.BlogEntry, error) {
 	var be models.BlogEntry
 	be, exists := bm.BlogEntries[path]
 	if !exists {
 		return be, ErrPageDoesNotExists
 	}
+
+	if !contentOnly {
+		content, err := afero.ReadFile(bm.Fs, be.Path)
+		if err != nil {
+			return be, fmt.Errorf("fail to read %s with error %v", path, err)
+		}
+		be.Content = content
+		return be, nil
+	}
+
 	file, err := bm.Fs.Open(be.Path)
 	if err != nil {
 		return be, fmt.Errorf("failed to read %s", path)
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
-	if contentOnly {
-		for i := 0; i < be.FrontMatter.ContentSidx; i++ {
-			_, err := reader.ReadString('\n')
-			if err != nil {
-				if err == io.EOF {
-					return be, fmt.Errorf("unexpected EOF for %s", path)
-				}
-				return be, fmt.Errorf("fail to read %s with error %v", path, err)
+	for i := 0; i < be.FrontMatter.ContentSidx; i++ {
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return be, fmt.Errorf("unexpected EOF for %s", path)
 			}
+			return be, fmt.Errorf("fail to read %s with error %v", path, err)
 		}
 	}
+
 	content, err := io.ReadAll(reader)
 	if err != nil {
 		return be, fmt.Errorf("fail to read content with error %v", err)

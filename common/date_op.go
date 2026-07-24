fe3edd5d52294c32049360890ba7d187d1e23af0
mushcatshiro <yingjieho@hotmail.com>
2026-07-24 08:20:37 +0800

    fix 3 layer date structure

--- common/date_op.go
+++ common/date_op.go
@@ -3,34 +3,43 @@ package common
 import (
 	"errors"
 	"fmt"
-	"log"
 	"time"
 )
 
+const DefaultTimeZone = "Asia/Singapore"
 const TimeLayout = "01-02-2006 15:04" // MM-DD-YYYY hh:mm
 const Html5TimeLayout = "2006-01-02T15:04"
 const BlogTimeLayout = "2006-01-02T15:04:05-07:00"
 const BlogTimeLayoutNoTtz = "2006-01-02 15:04"
 
-func ParseStringDate(value string, html5, blog bool) *time.Time {
-	if value == "" {
-		return nil
-	}
+func ParseStringDate(value, timezone string) (time.Time, error) {
 	var parsed time.Time
-	var err error
-	if html5 {
-		parsed, err = time.Parse(Html5TimeLayout, value)
-	} else if blog {
-		parsed, err = time.Parse(BlogTimeLayout, value)
-	} else {
-		parsed, err = time.Parse(TimeLayout, value)
+	if value == "" {
+		return parsed, nil
 	}
+	var tz string
+	if timezone == "" {
+		tz = DefaultTimeZone
+	}
+	loc, err := time.LoadLocation(tz)
 	if err != nil {
-		log.Printf("unexpected format %s results in %v", value, err)
-		return nil
+		return parsed, nil
+	}
+	parsed, err = time.ParseInLocation(Html5TimeLayout, value, loc)
+	if err != nil {
+		return parsed, err
+	}
+	return parsed.UTC(), nil
+}
+
+func MustParseStringDate(value, timezone string) *time.Time {
+	parsed, err := ParseStringDate(value, timezone)
+	if err != nil {
+		panic(err)
 	}
 	return &parsed
 }
+
 
 func ParseTimeToString(value *time.Time, html5 bool) string {
 	if value != nil {
